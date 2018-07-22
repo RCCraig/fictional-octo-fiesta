0ec6202bb4b1701e2ca3fdca79bab999c8c17207
rc3chambers <rc3chambers@gmail.com>
2018-07-22 12:42:40 -0400

    added nav partial and groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_211740) do
+ActiveRecord::Schema.define(version: 2018_07_22_164049) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -25,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_07_19_211740) do
     t.string "place"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "group_id"
   end
 
 end
