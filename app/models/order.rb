c61802887195ce488b5cc6bd61fc4411be3f42a5
rc3chambers <rc3chambers@gmail.com>
2018-07-23 13:27:24 -0400

    added groups controller and index

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  belongs_to :group
   has_many :items, dependent: :destroy, inverse_of: :order
   accepts_nested_attributes_for :items, allow_destroy: true, reject_if: ->(attrs) { attrs['name'].blank? || attrs['details'].blank? }
 end
